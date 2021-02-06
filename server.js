a74ac38af01e2bf603201e97a25e430244498f30
Brady Peterson <bpeterson1663@gmail.com>
2021-02-05 22:27:53 -0600

    feat(rest file): create rest file to test login route

--- server.js
+++ server.js
@@ -1,4 +1,33 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+const posts = [
+    {
+        username: 'Brady',
+        title: 'Post 1'
+    },
+    {
+        username: 'Jax',
+        title: 'Post 2'
+    }
+]
+
+app.get('/posts', (req, res) => {
+    res.json(posts)
+})
+
+app.post('/login', (req, res) => {
+    //Normally you would authenticate first
+    const username = req.body.username
+    const user = { name: username }
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken: accessToken})
+})
 
 app.listen(3000)
