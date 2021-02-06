2503a3aaf321ace9ddaf865554684c498c730321
Brady Peterson <bpeterson1663@gmail.com>
2021-02-05 22:57:32 -0600

    feat(refresh token): implement refresh token and logout

--- authServer.js
+++ authServer.js
@@ -7,12 +7,37 @@ const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 
+let refreshTokens = [] //usually stored in a database
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token
+    if (!refreshToken) return res.sendStatus(401)
+
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({name: user.name})
+        res.json({accessToken: accessToken})
+    })
+})
+
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
+})
+
 app.post('/login', (req, res) => {
     //Normally you would authenticate first
     const username = req.body.username
     const user = { name: username }
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({accessToken: accessToken})
+    const accessToken = generateAccessToken(user)
+    const refershToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refershToken)
+    res.json({accessToken: accessToken, refershToken: refershToken})
 })
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15s'}) // Usually longer , 10 to 15 minutes
+}
 
 app.listen(4000)
